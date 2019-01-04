c6a3dcac4fe2c9eaded5e01e02c809df4288f1da
Sira1 <bukethisim07@gmail.com>
2019-01-04 10:37:59 +0300

    UserManage class'ına SignUp metodu eklendi

--- BLL/UserManage.cs
+++ BLL/UserManage.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Entity.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL
 {
-    class UserManage
+    public class UserManage
     {
+        public void SıgnUp(User newUser)
+        {
+            SqlParameter p1 = new SqlParameter("UserName", newUser.UserName);
+            SqlParameter p2 = new SqlParameter("Name", newUser.Name);
+            SqlParameter p3 = new SqlParameter("SurName", newUser.Surname);
+            SqlParameter p4 = new SqlParameter("Password", newUser.Password);
 
+        }
     }
 }
