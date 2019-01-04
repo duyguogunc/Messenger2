a6ec33287ab0ba0c5f95224069bc7b29e3e86389
Sira1 <bukethisim07@gmail.com>
2019-01-04 10:12:31 +0300

    Message Class'ı için güncelleme

--- Entity/Models/Message.cs
+++ Entity/Models/Message.cs
@@ -8,9 +8,11 @@ namespace Entity.Models
 {
     public class Message
     {
+        public int MessageId { get; set; }
         public DateTime SendDate { get; set; }
         public string Content { get; set; }
         public User User { get; set; }
+        public List<User> ReciverUsers { get; set; }
         public Message()
         {
             SendDate = DateTime.Today;
