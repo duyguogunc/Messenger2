08ee065a4d0c4b42e84c579904ddc8a13d2c551e
Sira5 <sahanmerve@hotmail.com>
2019-01-04 11:35:17 +0200

    Dil seçeneği eklendi

--- Messenger/Giris.cs
+++ Messenger/Giris.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Entity.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,13 +21,22 @@ namespace Messenger
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            User u = new User();
+            u.UserName = txt_KullaniciAdi.Text;
+            u.Password = txt_sifre.Text;
+            UserManage us = new UserManage();
+            us.SignIn(u);
             new Form1().Show();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             new KayitOl().Show();
+        }
+
+        private void iNGİLİZCEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Messenger/KayitOl.Designer.cs
+++ Messenger/KayitOl.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KayitOl));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -42,169 +43,108 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txt_sifretekrar = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_fotosec = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(31, 59);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 18);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Adınız:";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(31, 92);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 18);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Soyadınız:";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(31, 119);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 18);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Kullanıcı Adı:";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(31, 150);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 18);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Şifre:";
             // 
             // txt_Ad
             // 
-            this.txt_Ad.Location = new System.Drawing.Point(195, 59);
+            resources.ApplyResources(this.txt_Ad, "txt_Ad");
             this.txt_Ad.Name = "txt_Ad";
-            this.txt_Ad.Size = new System.Drawing.Size(170, 22);
-            this.txt_Ad.TabIndex = 0;
             // 
             // txt_soyad
             // 
-            this.txt_soyad.Location = new System.Drawing.Point(195, 87);
+            resources.ApplyResources(this.txt_soyad, "txt_soyad");
             this.txt_soyad.Name = "txt_soyad";
-            this.txt_soyad.Size = new System.Drawing.Size(170, 22);
-            this.txt_soyad.TabIndex = 1;
             // 
             // txt_KullaniciAdi1
             // 
-            this.txt_KullaniciAdi1.Location = new System.Drawing.Point(195, 115);
+            resources.ApplyResources(this.txt_KullaniciAdi1, "txt_KullaniciAdi1");
             this.txt_KullaniciAdi1.Name = "txt_KullaniciAdi1";
-            this.txt_KullaniciAdi1.Size = new System.Drawing.Size(170, 22);
-            this.txt_KullaniciAdi1.TabIndex = 2;
             // 
             // txt_sifre
             // 
-            this.txt_sifre.Location = new System.Drawing.Point(195, 143);
+            resources.ApplyResources(this.txt_sifre, "txt_sifre");
             this.txt_sifre.Name = "txt_sifre";
-            this.txt_sifre.PasswordChar = '*';
-            this.txt_sifre.Size = new System.Drawing.Size(170, 22);
-            this.txt_sifre.TabIndex = 3;
             // 
             // btn_kaydol
             // 
+            resources.ApplyResources(this.btn_kaydol, "btn_kaydol");
             this.btn_kaydol.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.btn_kaydol.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btn_kaydol.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btn_kaydol.Location = new System.Drawing.Point(227, 256);
             this.btn_kaydol.Name = "btn_kaydol";
-            this.btn_kaydol.Size = new System.Drawing.Size(124, 57);
-            this.btn_kaydol.TabIndex = 7;
-            this.btn_kaydol.Text = "Kaydol";
             this.btn_kaydol.UseVisualStyleBackColor = false;
             this.btn_kaydol.Click += new System.EventHandler(this.btn_kaydol_Click);
             // 
             // rd_kadin
             // 
-            this.rd_kadin.AutoSize = true;
-            this.rd_kadin.Location = new System.Drawing.Point(180, 213);
+            resources.ApplyResources(this.rd_kadin, "rd_kadin");
             this.rd_kadin.Name = "rd_kadin";
-            this.rd_kadin.Size = new System.Drawing.Size(65, 21);
-            this.rd_kadin.TabIndex = 5;
             this.rd_kadin.TabStop = true;
-            this.rd_kadin.Text = "Kadın";
             this.rd_kadin.UseVisualStyleBackColor = true;
             // 
             // rd_erkek
             // 
-            this.rd_erkek.AutoSize = true;
-            this.rd_erkek.Location = new System.Drawing.Point(268, 213);
+            resources.ApplyResources(this.rd_erkek, "rd_erkek");
             this.rd_erkek.Name = "rd_erkek";
-            this.rd_erkek.Size = new System.Drawing.Size(65, 21);
-            this.rd_erkek.TabIndex = 6;
             this.rd_erkek.TabStop = true;
-            this.rd_erkek.Text = "Erkek";
             this.rd_erkek.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(33, 216);
+            resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(99, 18);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Cinsiyetiniz:";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(31, 179);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(102, 18);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Şifre Tekrar:";
             // 
             // txt_sifretekrar
             // 
-            this.txt_sifretekrar.Location = new System.Drawing.Point(195, 175);
+            resources.ApplyResources(this.txt_sifretekrar, "txt_sifretekrar");
             this.txt_sifretekrar.Name = "txt_sifretekrar";
-            this.txt_sifretekrar.PasswordChar = '*';
-            this.txt_sifretekrar.Size = new System.Drawing.Size(170, 22);
-            this.txt_sifretekrar.TabIndex = 4;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(398, 59);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(112, 106);
-            this.pictureBox1.TabIndex = 14;
-            this.pictureBox1.TabStop = false;
             // 
             // btn_fotosec
             // 
+            resources.ApplyResources(this.btn_fotosec, "btn_fotosec");
             this.btn_fotosec.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.btn_fotosec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btn_fotosec.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btn_fotosec.Location = new System.Drawing.Point(398, 256);
             this.btn_fotosec.Name = "btn_fotosec";
-            this.btn_fotosec.Size = new System.Drawing.Size(124, 57);
-            this.btn_fotosec.TabIndex = 15;
-            this.btn_fotosec.Text = "Fotoğraf Seç";
             this.btn_fotosec.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
             // 
             // KayitOl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(546, 331);
             this.Controls.Add(this.btn_fotosec);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
@@ -222,7 +162,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "KayitOl";
-            this.Text = "KayitOl";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
