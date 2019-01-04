eba0abe1d97eb0072d57878d5a52b439f6ab2e52
Sira5 <sahanmerve@hotmail.com>
2019-01-04 13:16:22 +0200

    message

--- Messenger/Form1.cs
+++ Messenger/Form1.cs
@@ -17,22 +17,41 @@ namespace Messenger
         {
             InitializeComponent();
         }
+        Entity.Models.Message m = new Entity.Models.Message();
+        MessageManager mm = new MessageManager();
 
         private void btn_Gonder_Click(object sender, EventArgs e)
         {
-            Entity.Models.Message m = new Entity.Models.Message();
-            MessageManager mm = new MessageManager();
+
             m.Content = rch_GidenYazi.Text;
             m.User = Program.User;
             mm.InsertMessage(m);
 
-            
-            
+
+
+        }
+        public void GetMessage()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            var gelen = mm.GetMessage();
+
+            foreach (var item in gelen)
+            {
+                Label l = new Label();
+                l.Text = item.Content;
+                flowLayoutPanel1.Controls.Add(l);
+
+            }
         }
 
-        private void rch_GelenYazi_TextChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetMessage();
         }
     }
 }
